7e1745e957df8145f5bbb2f6b9c924bdb46a906c
Antonio Brito <edilsonlm217@hotmail.com>
2023-03-25 19:54:14 +0000

    Implementação de tipagem para Tenant com informações do banco de dados

---
+++ src/app/models/DetailedTenant.ts
@@ -0,0 +1,23 @@
+export interface DetailedTenant {
+    id: string,
+    cnpj: string,
+    responsavel: string,
+    contato: string,
+    provedor: {
+        nome: string
+    },
+    database: {
+        conectado: boolean
+        name: string
+		dialect: string
+		host: string
+		username: string
+		password: string
+    },
+    assinatura: {
+        ativa: boolean,
+        valor: number,
+        data_vencimento: Date,
+        dia_vencimento: string
+    }
+}
