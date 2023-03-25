36df24fd7da357b58b1ee6d650a3bc10e1eaa7bd
Antonio Brito <edilsonlm217@hotmail.com>
2023-03-25 19:47:06 +0000

    Criação das rotas cadastrar, editar, receber

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditarComponent } from './pages/editar/editar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CadastrarComponent } from './pages/cadastrar/cadastrar.component';
+import { ReceberComponent } from './pages/receber/receber.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'editar', component: EditarComponent },
+  { path: 'cadastrar', component: CadastrarComponent },
+  { path: 'receber', component: ReceberComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
