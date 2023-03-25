c694b55f557aa410e61b4917b00658aecac4109d
Antonio Brito <edilsonlm217@hotmail.com>
2023-03-25 20:22:56 +0000

    Criação do card de sibscriptions

---
+++ src/app/components/subscription-card/subscription-card.component.ts
@@ -0,0 +1,56 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { Tenant } from 'src/app/models/Tenant';
+import { parseISO, format } from 'date-fns';
+
+@Component({
+  selector: 'app-subscription-card',
+  templateUrl: './subscription-card.component.html',
+  styleUrls: ['./subscription-card.component.scss']
+})
+export class SubscriptionCardComponent {
+  @Input() tenant!: Tenant;
+
+  constructor(
+    public api: ApiService,
+    public router: Router,
+  ) { }
+
+  async toggleIsActive(): Promise<void> {
+    try {
+      const updatedTenant = await this.api.toggleIsActive(this.tenant.cnpj);
+      this.tenant = updatedTenant;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async connectOnClick(): Promise<void> {
+    try {
+      this.tenant = await this.api.connectDatabase(this.tenant.cnpj);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  wppOnClick(): void {
+    window.open('https://whatsa.me/5592991424261', '_blank');
+  }
+
+  handleRecebimento(): void {
+    this.router.navigate(['/receber'], {
+      queryParams: {
+        tenantId: this.tenant.id
+      }
+    });
+  }
+
+  handleEditing(): void {
+    this.router.navigate(['/editar'], {
+      queryParams: {
+        tenantId: this.tenant.id
+      }
+    });
+  }
+}
