981a4a179ad28b88bbac3bb17522cbb2636ba3b9
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2023-03-25 17:02:46 -0400

    Adição de documentação

--- src/app/services/tenant.service.ts
+++ src/app/services/tenant.service.ts
@@ -10,10 +10,20 @@ export class TenantService {
 
   constructor() { }
 
+  /**
+   * Sets the tenants array to the provided array of Tenant objects.
+   *
+   * @param tenants An array of Tenant objects to set as the new tenants array.
+   */
   setTenants(tenants: Tenant[]) {
     if (tenants) { this.tenants = tenants; }
   }
 
+  /**
+   * Retrieves a tenant object by its ID.
+   * @param id - The ID of the tenant to retrieve.
+   * @returns The tenant object if found, otherwise null.
+   */
   getTenantById(id: string): Tenant | null {
     if (id && id !== '') {
       const [tenant] = this.tenants.filter(tenant => tenant.id === id);
