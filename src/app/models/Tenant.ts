be85ad4fdb237e0253c9432051fcf5d9af39e5ee
Antonio Brito <edilsonlm217@hotmail.com>
2023-03-25 19:54:42 +0000

    Implementação de tipagem para Tenant sem informações do banco de dados

---
+++ src/app/models/Tenant.ts
@@ -0,0 +1,18 @@
+export interface Tenant {
+    id: string,
+    cnpj: string,
+    responsavel: string,
+    contato: string,
+    provedor: {
+        nome: string
+    },
+    database: {
+        conectado: boolean
+    },
+    assinatura: {
+        ativa: boolean,
+        valor: number,
+        data_vencimento: Date,
+        dia_vencimento: string
+    }
+}
