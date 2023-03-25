1f40647520cedb83f2b91e7598736411b27aabb3
Antonio Brito <edilsonlm217@hotmail.com>
2023-03-25 20:26:20 +0000

    Implementação de formulários virtual

---
+++ src/app/pages/editar/editar-model/index.ts
@@ -0,0 +1,19 @@
+export class EditarForm {
+    cnpj!: string;
+    responsavel!: string;
+    contato!: string;
+
+    provedorNome!: string;
+
+    databaseName!: string;
+    databaseDialect!: string;
+    databaseHost!: string;
+    databaseUsername!: string;
+    databasePassword!: string;
+
+    assinaturaValor!: number;
+    assinaturaDataVencimento!: string;
+    assinaturaDiaVencimento!: string;
+
+    constructor() { }
+}
