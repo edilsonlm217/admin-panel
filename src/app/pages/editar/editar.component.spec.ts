fe6b7af8cfc8160d38aafa7c99dc976b99dc2ff9
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2023-03-26 10:46:17 -0400

    Criação de testes unitários

--- src/app/pages/editar/editar.component.spec.ts
+++ src/app/pages/editar/editar.component.spec.ts
@@ -1,17 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormsModule } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { of } from "rxjs";
+import { ApiService } from "src/app/services/api.service";
+import { TenantService } from "src/app/services/tenant.service";
+import { EditarFormComponent } from "./editar-form/editar-form.component";
+import { EditarComponent } from "./editar.component";
 
-import { EditarComponent } from './editar.component';
-
-describe('EditarComponent', () => {
+fdescribe('EditarComponent', () => {
   let component: EditarComponent;
   let fixture: ComponentFixture<EditarComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EditarComponent ]
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        EditarComponent,
+        EditarFormComponent
+      ],
+      providers: [
+        { provide: ActivatedRoute, useValue: { queryParams: of({ tenantId: 'testId' }) } },
+        { provide: ApiService, useValue: {} },
+        { provide: TenantService, useValue: {} },
+      ],
+      imports: [FormsModule]
     })
-    .compileComponents();
+      .compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(EditarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +35,14 @@ describe('EditarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set tenantId when setTenantId is called', () => {
+    component.setTenantId('testId');
+    expect(component.tenantId).toEqual('testId');
+  });
+
+  it('should set tenantId when queryParams have tenantId', () => {
+    expect(component.tenantId).toEqual('testId');
   });
 });
